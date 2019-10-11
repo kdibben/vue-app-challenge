7081d148d00aaca4b5a99764c021788e23b7f74e
Kelsie Dibben <kmdibben@gmail.com>
2019-10-11 12:40:31 -0500

    Added one more test

--- test/calculations.test.js
+++ test/calculations.test.js
@@ -4,4 +4,5 @@ QUnit.test('TEST calculateTasks', assert => {
     assert.equal(calculateTasks(4, 5), 20, 'All Positive')
     assert.equal(calculateTasks(-9, -7), 63, 'All Negative')
     assert.equal(calculateTasks(-8, 3), -24, 'One Negative, One Positive')
+    assert.equal(calculateTasks(400, 200), 80000, 'Big Numbers')
   });
